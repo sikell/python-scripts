392634600c21fbea7e90e61f11f8709c1844b183
Simon Keller <simkelle@htwg-konstanz.de>
2018-09-22 17:17:22 +0200

    script to extract links of a website

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-scripts
 A collection of some useful python scripts in the world of an information scientist.
+
+## extractUrls.py
+A script to extract links of a website. Can be used to filter and download multiple files from a html page.
---
+++ extractUrls.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+parser = argparse.ArgumentParser(description='Extract link urls of a website.')
+parser.add_argument('--url', help='url of website to parse')
+args = parser.parse_args()
+
+if args.url is not None:
+    url = args.url
+else:
+    url = "http://www.python.org"
+response = requests.get(url)
+# parse html
+page = str(BeautifulSoup(response.content, "html.parser"))
+
+
+def getURL(page):
+    """
+
+    :param page: html of web page (here: Python home page) 
+    :return: urls in that page 
+    """
+    start_link = page.find("a href")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
+
+while True:
+    url, n = getURL(page)
+    page = page[n:]
+    if url:
+        print(url)
+    else:
+        break
