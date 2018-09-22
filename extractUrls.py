d279c06159587bf863e567a2a502f98583db2272
Simon Keller <simkelle@htwg-konstanz.de>
2018-09-22 17:39:32 +0200

    distinct extracted urls by command line arg

--- extractUrls.py
+++ extractUrls.py
@@ -6,21 +6,24 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Extract link urls of a website.')
 parser.add_argument('--url', help='url of website to parse')
+parser.add_argument('--distinct', action='store_true', help='result list should not contain duplicate link urls, do not preserve order')
 args = parser.parse_args()
 
 if args.url is not None:
     url = args.url
 else:
     url = "http://www.python.org"
+
+distinct = args.distinct
+
 response = requests.get(url)
 # parse html
 page = str(BeautifulSoup(response.content, "html.parser"))
 
-
 def getURL(page):
     """
-
-    :param page: html of web page (here: Python home page) 
+    Extract next url from page.
+    :param page: html of web page 
     :return: urls in that page 
     """
     start_link = page.find("a href")
@@ -31,10 +34,17 @@ def getURL(page):
     url = page[start_quote + 1: end_quote]
     return url, end_quote
 
+urls = list()
 while True:
     url, n = getURL(page)
     page = page[n:]
     if url:
-        print(url)
+        urls.append(url)
     else:
         break
+
+if distinct:
+    urls = set(urls)
+
+for url in urls:
+    print(url)
