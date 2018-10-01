5f90501f80ed19d33f091b20d476a555c1315630
Simon Keller <simkelle@htwg-konstanz.de>
2018-10-01 07:10:46 +0200

    handle errors and separate foreign domain links

--- sitemapBuilder.py
+++ sitemapBuilder.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 from urllib.request import urlretrieve, urlopen, Request
 from urllib.parse import unquote, urljoin, urlparse
+from urllib.error import HTTPError, URLError
 from os import path, makedirs
 
 parser = ArgumentParser(description='Extract link urls of a website.')
@@ -26,6 +27,10 @@ class FuncThread(threading.Thread):
  
     def run(self):
         self._target(*self._args)
+
+class Error():
+    def __init__(self, message):
+        self._message = message
 
 def get_url(page):
     """Extract next url from page.
@@ -74,9 +79,13 @@ def open_url(url):
         request = Request(url)
         request.get_method = lambda : 'HEAD'
         response = urlopen(request)
+    except HTTPError as error:
+        return Error(str(error.code) + " - " + error.reason)
+    except URLError as error:
+        return Error(error.reason)
     except:
         print("Could not access URL with HEAD " + url)
-        return None
+        return Error("Unknown Exception")
     http_message = response.info()
     if http_message.get_content_type() != "text/html":
         print("Wrong content type: " + http_message.get_content_type())
@@ -97,49 +106,51 @@ def open_url(url):
     response.close()
     return page
 
-thread_lock = threading.Lock()
-
-def process_url(start_url, processed_urls, error_urls):
+def process_url(start_url, processed_urls, error_urls, foreign_hosts, thread_lock):
+    print("-> " + start_url)
     thread_lock.acquire()
+    if start_url in processed_urls:
+        thread_lock.release()
+        return
     processed_urls.add(start_url)
     thread_lock.release()
-    print("-> " + start_url)
+
     page = open_url(start_url)
-    if page is None:
+    if isinstance(page, Error):
         # error while request
         thread_lock.acquire()
-        error_urls.add(start_url)
-        processed_urls.remove(start_url)
+        error_urls.add(start_url + " >>>> " + page._message)
         thread_lock.release()
         return
     if page is False: 
         # of wrong content type
         return
+
     sub_threads = list()
     for url in find_urls_in_page(page):
         hostname = urlparse(url).netloc
         if hostname is "" or hostname == host:
             constructed_url = urljoin(start_url, url)
-            thread_lock.acquire()
-            not_already_processed = constructed_url not in processed_urls
-            thread_lock.release()
-            if not_already_processed:
-                subThread = FuncThread(process_url, constructed_url, processed_urls, error_urls)
-                sub_threads.append(subThread)
-                subThread.start()
+            subThread = FuncThread(process_url, constructed_url, processed_urls, error_urls, foreign_hosts, thread_lock)
+            sub_threads.append(subThread)
+            subThread.start()
         else:
             print("Other host: " + url)
-            error_urls.add(url)
+            foreign_hosts.add(url)
     for t in sub_threads:
         t.join()
 
 
-urls = set()
+processed_urls = set()
 error_urls = set()
-tMain = FuncThread(process_url, host + "/", urls, error_urls)
+foreign_hosts = set()
+thread_lock = threading.Lock()
+
+tMain = FuncThread(process_url, host + "/", processed_urls, error_urls, foreign_hosts, thread_lock)
 tMain.start()
 tMain.join()
 
 if save_to_file:
-    write_to_file(urls, "urls")
+    write_to_file(processed_urls, "urls")
+    write_to_file(foreign_hosts, "foreign")
     write_to_file(error_urls, "errors")
